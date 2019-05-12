f5c93515cfb6db3a8c22315eefa68ddd7350eada
Dacilla <alex@gartonhill.com>
2019-05-12 11:21:05 +1000

    Added more functions and classes

---
+++ src/main/java/com/mycompany/atmimplementation/ATM.java
@@ -0,0 +1,19 @@
+package com.mycompany.atmimplementation;
+
+public class ATM {
+    private Long id;
+    private String location;
+    
+    public Boolean logIn(){
+        return true;
+    }
+    
+    public void showMenu(){
+    }
+    
+    public void logOut(){
+    }
+    
+    public void showDetails(){
+    }
+}
